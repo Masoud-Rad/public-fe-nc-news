81512a30905a464626b9146f0c3a77ab3bbcaf43
Masoud <masoud.rad.uk@gmail.com>
2023-05-30 16:29:03 +0100

    last change1

---
+++ FE_NC_news/src/App.jsx
@@ -0,0 +1,23 @@
+
+
+import Header from './components/Header';
+import Nav from './components/Nav';
+import ArticlesList from './components/ArticlesList'
+import TopicList from './components/TopicList'
+import Home from './components/Home'
+function App() {
+ 
+
+  return (
+    <>
+      <Header />
+      <Nav/>
+      <ArticlesList />
+      <TopicList />
+      <Home />
+
+    </>
+  )
+}
+
+export default App
