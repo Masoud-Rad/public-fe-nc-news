5c880d26fe80a0869cbe45a0a3b322ae54748538
Masoud <masoud.rad.uk@gmail.com>
2023-05-31 14:10:04 +0100

    task6 is done

--- FE_NC_news/src/utils.js
+++ FE_NC_news/src/utils.js
@@ -20,33 +20,31 @@ export const fetchArticles = ()=>{
   }
 
 
-  // export const fetchArticles = ()=>{
-
-  //   return  fetch(`https://nc-news-os80.onrender.com/api/articles`).then((res) => {
-  //         console.log(res.json()) 
-  //     }).catch(err=>console.log(err))
-  // }
 
   export const fetchArticleById = (article_id)=>{
 
 
     return  ncNewsApi.get(`/articles/${article_id}`)
     .then((response)=> {
-      console.log(response.data)
       return(response.data);
     })
     .catch((error) =>{
       console.log(error);
     })
-  
+ 
+  }
 
-    
 
-      // return  fetch(`https://nc-news-os80.onrender.com/api/articles/${article_id}`).then((res) => {
-      //       return res.json()
-      //   })
-    
-    
+  //-------------------------------------------Comments----------------------
 
+  export const fetchComments= (article_id)=>{
+
+    return  ncNewsApi.get(`/articles/${article_id}/comments`)
+    .then((response)=> {
+      return(response.data);
+    })
+    .catch((error) =>{
+      console.log(error);
+    })
 
   }
