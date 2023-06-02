abde67d8244482d7ba7867f6451fb37f6c331f92
Masoud <masoud.rad.uk@gmail.com>
2023-06-02 16:58:46 +0100

    comment's votes works now

---
+++ FE_NC_news/src/components/CommentsVotes.jsx
@@ -0,0 +1,24 @@
+import { patchComment} from '../utils'
+
+import { useState , useEffect} from 'react'
+
+function CommentsVotes({votes, commentId}){
+    const [commentVoteChanges, setCommentVoteChanges] =useState(0)
+
+    function incCommentVotes(comment_id){
+    
+        
+        setCommentVoteChanges((currVotes)=>{return currVotes+1} );
+    
+    
+    
+
+    patchComment(comment_id, { inc_votes : 1 })
+  }
+
+
+return <button  onClick={()=>{incCommentVotes(commentId)}}> votes: {votes+commentVoteChanges}</button>
+
+}
+
+export default CommentsVotes;
